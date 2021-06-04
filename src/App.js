eb5d213a1e0c4ef001ff0480f7bfc1e3dac85386
lenx0 <thiagofreitas1106@gmail.com>
2021-06-04 18:17:26 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,37 +1,70 @@
+import { useState } from 'react';
 import './App.css';
 import GithubImage from './GitHub-Logo.png'
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    fetch(`https://api.github.com/users/${search}`)
+      .then(response => response.json())
+      .then(userResponse => setUserData(userResponse));
+  }
+
+  const handleChange = (event) => {
+    setSearch(event.target.value);
+
+  }
+
   return (
     <div className="container text-center">
       <h1 className="py-5 text-uppercase">Github profile</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <div className="input-group">
             <input
               type="text"
               className="form-control"
               required
+              value={search}
+              onChange={handleChange}
             />
             <span className="input-group-btn">
               <button type="submit" className="btn btn-success">
                 Search
-
                 </button>
             </span>
           </div>
         </div>
       </form>
       <div className="py-5">
-        <img src={GithubImage}
-          className="responsive rounded circle"
-          alt=""
-          height="200px"
-        />
-        <h1 className="pt-3">
-          <a href="https://github.com/lenx0" target="_new">
-            Thiago Beraldo</a>
-        </h1>
+        {!userData && (
+          <img src={GithubImage}
+            className="responsive rounded circle"
+            alt=""
+            height="200px"
+          />
+        )}
+        {userData && (
+          <div>
+            <img
+              src={userData.avatar_url}
+              className="responsive rounded-circle"
+              alt=""
+              height="200px"
+            />
+            <h1 className="pt-2">
+              <a href="https://github.com/lenx0" target="_new">
+                {userData.name}
+              </a>
+            </h1>
+            <h3>
+              {userData.location}
+            </h3>
+          </div>
+        )}
       </div>
     </div>
 
