b37bf2e27bf1e1ab0792e1badcc18d8e0a76f886
lenx0 <thiagofreitas1106@gmail.com>
2021-06-04 12:12:44 -0300

    static github link

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import GithubImage from './GitHub-Logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="py-5 text-uppercase">Github profile</h1>
+      <form>
+        <div className="form-group">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              required
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-success">
+                Search
+
+                </button>
+            </span>
+          </div>
+        </div>
+      </form>
+      <div className="py-5">
+        <img src={GithubImage}
+          className="responsive rounded circle"
+          alt=""
+          height="200px"
+        />
+        <h1 className="pt-3">
+          <a href="https://github.com/lenx0" target="_new">
+            Thiago Beraldo</a>
+        </h1>
+      </div>
     </div>
+
   );
+
+
 }
+
+
+
+
 
 export default App;
